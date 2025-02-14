2daaac989fa017dd36f55dba5c1cbc631f3e166c
Erudite-pratham <pratham.consultadd@gmail.com>
2025-02-14 12:49:54 -0500

    Session9

---
+++ Microservices/ApiGateway/src/test/java/com/training/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
